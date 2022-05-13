631480ce1bc546fc637b1c4625f29c1a9be1baad
IronWesso <IronWesso>
2022-05-13 11:00:35 -0400

    Zippy

---
+++ calculadora.frontend/javascript/funciones.js
@@ -0,0 +1,36 @@
+#1{
+    onclick="document.Calculadora.ans.value+='1'";
+ }
+
+ #2{
+    onclick="document.Calculadora.ans.value+='2'";
+ }
+
+ 
+ #3{
+    onclick="document.Calculadora.ans.value+='3'";
+ }
+
+ #4{
+    onclick="document.Calculadora.ans.value+='4'";
+ }
+
+ #5{
+    onclick="document.Calculadora.ans.value+='5'";
+ }
+
+ #6{
+    onclick="document.Calculadora.ans.value+='6'";
+ }
+
+ #7{
+    onclick="document.Calculadora.ans.value+='7'";
+ }
+
+ #8{
+    onclick="document.Calculadora.ans.value+='8'";
+ }
+
+ #9{
+    onclick="document.Calculadora.ans.value+='9'";
+ }
