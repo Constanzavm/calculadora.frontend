e28f81f7a2f59bd2497e62e98028273d3125be9e
IronWesso <104546781+IronWesso@users.noreply.github.com>
2022-05-13 12:06:30 -0400

    Update calculadora.js

--- calculadora.frontend/js/calculadora.js
+++ calculadora.frontend/js/calculadora.js
@@ -1,15 +1,4 @@
-function accion(valor) {
-    
-    
-    if (valor == "Limpiar"){
-        document.getElementById("tResultado").value = "";
-    } else if (valor == "Resultado") {
-        document.getElementById("tResultado").value = eval(document.getElementById("tResultado").value);
-    } else if (!isNaN(valor) || valor == "+" || valor == '-' || valor == '*' || valor == '/'){
-        document.getElementById("tResultado").value += valor;
 
-
-    }
 
 function siete(){
     document.Calculadora.ans.value+='7'
